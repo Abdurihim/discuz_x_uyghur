f44166145502eb0fcb7ebd29a4bcfe59158662fb
FinalFantasia <finalfantasy@finalfantasia.com>
2014-02-09 00:22:29 -0800

    initial commit

---
+++ source/language/ulp_version.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *      为使用需要而翻译，原程序开发者版权所有
+ *      Translated By Nurqut Team.
+ *      Translation Time : 2014-01-02
+*/
+
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+if(!defined('ULP_VERSION')) {
+	define('ULP_VERSION', 'X3.1');
+	define('ULP_RELEASE', '20140205');
+}
+
+?>
